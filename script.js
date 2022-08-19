024982587280f6708b1593ddb14605e9fbf4dcd9
Cam Acma <c.avantm92@gmail.com>
2022-08-19 12:39:12 -0500

    Add createGrid function

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-//Color wheel
-var ColorPicker = VueColorPicker;
-let chosenHue = null;
-Vue.createApp({
-    components: {
-        ColorPicker: ColorPicker,
-    },
-    setup() {
-        const color = Vue.reactive({
-            hue: 50,
-            saturation: 100,
-            luminosity: 50,
-            alpha: 1,
-        });
-        return {
-            color
-        };
-    },
-}).mount('#app');
+//Color select
+let userColor = null;
+var colorPicker = new iro.ColorPicker("#picker", {
+    width: 280,
+    margin: 20,
+    sliderSize: 10
+  });
+colorPicker.on('color:change', function(color) {
+    userColor = colorPicker.color.hexString;
+    console.log(userColor)
+});
 
 //Toast
 const toastTrigger = document.getElementById('hintBtn')
@@ -27,3 +19,45 @@ if (toastTrigger) {
     toast.show()
   })
 }
+
+let gridSize = 16;
+
+$('document').ready(function(){
+    //BW select
+    $('#user-black-btn').click(function(){
+        userColor = '#000000';
+        console.log(userColor);
+    });
+    $('#user-white-btn').click(function(){
+        userColor = '#ffffff';
+        console.log(userColor);
+    });
+
+    //Drawing grid
+    function createGrid() {
+        for(let i = 1; i <= gridSize; i++) {
+            $('#drawing-zone').append('<div></div>');
+            $(`#drawing-zone div:nth-child(${i})`)
+            .addClass('row')
+            .addClass('row-cols-auto')
+            .addClass('d-flex')
+            .addClass('flex-nowrap')
+            .attr('id', `drawing-row-${i}`);
+            console.log(`row ${i} created`)
+        }
+        for(let i = 1; i <= gridSize; i++) {
+            for(let j = 1; j <= gridSize; j++){
+                $(`#drawing-row-${i}`).append('<div></div>');
+                $(`#drawing-row-${i} div:nth-child(${j})`).addClass('col').addClass(`draw-square-16`);
+            }
+        }
+
+    }
+
+
+
+    createGrid();
+});
+
+
+
