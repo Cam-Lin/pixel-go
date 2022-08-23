a4c37f6751328d707aab8398529bcad8223057da
Cam Acma <c.avantm92@gmail.com>
2022-08-22 20:37:23 -0500

    Add paint functionality on mousedown and mouseover, add function to #clear-btn

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//Color select
-let userColor = null;
+//Color picker
+let userColor = '#000';
 var colorPicker = new iro.ColorPicker("#picker", {
     width: 280,
     margin: 20,
@@ -11,15 +11,19 @@ colorPicker.on('color:change', function(color) {
 });
 
 //Toast
-const toastTrigger = document.getElementById('hintBtn')
-const toastLiveExample = document.getElementById('hint')
+
+let hintList = ['Go to Adobe Color to find color palettes', 'Start with something simple', 'Start with the outline and add color later', 'Copy designs you like to learn how they work'];
+
+const toastTrigger = document.getElementById('hintBtn');
+const toastLiveExample = document.getElementById('hint');
 if (toastTrigger) {
-  toastTrigger.addEventListener('click', () => {
-    const toast = new bootstrap.Toast(toastLiveExample)
-    toast.show()
+    toastTrigger.addEventListener('click', () => {
+    let hintPrint = hintList[Math.floor(Math.random() * 4)];
+    $('#toast-message').text(`${hintPrint}`);
+    const toast = new bootstrap.Toast(toastLiveExample);
+    toast.show();
   })
 }
-
 
 $('document').ready(function(){
     //BW select
@@ -34,6 +38,9 @@ $('document').ready(function(){
 
     //Drawing grid
     let gridSize = 16; //Default size loading the page
+    let idArr = []; //Ids to be passed on to Painting jQuery selector later
+    let idStr = null;
+
     function createGrid() {
         for(let i = 1; i <= gridSize; i++) {
             $('#drawing-zone').append('<div></div>');
@@ -47,27 +54,46 @@ $('document').ready(function(){
         for(let i = 1; i <= gridSize; i++) {
             for(let j = 1; j <= gridSize; j++){
                 $(`#drawing-row-${i}`).append('<div></div>');
-                $(`#drawing-row-${i} div:nth-child(${j})`).addClass('col').addClass(`draw-square`).addClass(`scaled-${gridSize}`);
+                $(`#drawing-row-${i} div:nth-child(${j})`).addClass('col').addClass(`draw-square`).addClass(`scaled-${gridSize}`).attr('id', `draw-square-${i}-${j}`);
+                idArr.push(`#draw-square-${i}-${j}`);
             }
         }
+        let idStr = idArr.join(', ');
+
+        //Paint
+        $(idStr).on('mousedown', function(){
+            console.log('test-click');
+            $(this).css('background', `${userColor}`);
+        });
+
+        $(idStr).on('mouseover', function(){
+            console.log('test-click');
+            $(this).css('background', `${userColor}`);
+        });
+
+        //Clear
+        $('#clear-btn').click(() =>{
+            $(idStr).css('background', '#ffffff')
+        });
     }
 
     //Grid size change
     $('#btn-radio-16').click(function(){
-        console.log(gridSize)
         $(`#drawing-zone`).empty();
         gridSize = 16;
         createGrid();
     });
     $('#btn-radio-24').click(function(){
-        console.log(gridSize)
         $(`#drawing-zone`).empty();
         gridSize = 24;
         createGrid();
+
     });
 
-    //Initial grid load
+    //Initial grid
+    $(`#drawing-zone`).addClass('animate__animated').addClass('animate__fadeIn');
     createGrid();
+ 
 });
 
 
